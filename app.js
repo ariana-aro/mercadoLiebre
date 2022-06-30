670716f016014ac8912744b95533754092bc0de8
ariana-aro <ariana_n_aro@hotmail.com>
2022-06-29 21:15:18 -0300

    lo dejo asi

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.set('puerto',process.env.PORT || 3000)
-app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
 
+app.listen(3001, ()=>{
+    console.log('Servidor funcionando');
+});
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');

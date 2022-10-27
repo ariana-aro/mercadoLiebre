60025148f7c9a7cb0500f0c49ac2010e39054ac0
ariana-aro <105684919+ariana-aro@users.noreply.github.com>
2022-10-27 19:27:03 -0300

    actualizo

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+const path = require("path");
 
+app.set('puerto', process.env.PORT || 3001);
+
+app.use("/public", express.static(__dirname + '/public'));
+
+app.set('puerto', process.env.PORT || 3001);
 
 app.listen(3001, ()=>{
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
+app.get("/", (req, res) => {
+    let htmlInfo = path.resolve(__dirname, "./views/home.html");
+    res.sendFile(htmlInfo);
+})
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
+app.get("/register", (req, res) => {
+    let htmlInfo = path.resolve(__dirname, "./views/register.html");
+    res.sendFile(htmlInfo);
+})
+
+app.get("/login", (req, res) => {
+    let htmlInfo = path.resolve(__dirname, "./views/login.html");
+    res.sendFile(htmlInfo);
+})
